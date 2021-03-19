c3dc95ab7ab400268605feca7df8a9977676433e
dev60 <dev60@winserver.paysquare.net>
2021-03-19 16:49:33 +0530

    preeti bra

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,15 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://localhost:8080/hrms/v1',
-  // baseUrl8082: 'http://localhost:8082/hrms/v1',
+  // baseUrl8080: 'http://192.168.1.162:8080/hrms/v1/',
+  // baseUrl8082: 'http://localhost:8082/hrms/v1/',
   // baseUrl8083: 'http://localhost:8083/hrms/v1/',
-  // baseUrl8080: 'http://deliziahruat.paysquare.com:8080/hrms/v1/',
+  // //baseUrl8080: 'http://deliziahruat.paysquare.com:8080/hrms/v1/',
   // // apiBaseUrl: 'http://PAYLAPTOP93:8085/hrms/v1/',
   // baseUrl8085: 'http://localhost:8085/hrms/v1/',
   // baseUrl8086:  'http://localhost:8086/hrms/v1/',
   // baseUrl8084:  'http://localhost:8084/hrms/v1/',
+  // baseUrl8087:  'http://localhost:8084/hrms/v1/',
 
     // baseUrl8080: 'http://dev.deliziahr.com:8080/hrms/v1/',
     // baseUrl8082: 'http://dev.deliziahr.com:8082/hrms/v1/',
