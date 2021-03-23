31ec429c6d50c39bb0073954d0857c5cdefffef4
takalkar_anant <anant.takalkar@paysquare.com>
2021-03-23 10:51:09 +0530

    Added change

--- src/environments/dist/environment.js
+++ src/environments/dist/environment.js
@@ -3,24 +3,6 @@ exports.__esModule = true;
 exports.environment = void 0;
 exports.environment = {
     production: false,
-<<<<<<< Updated upstream
-    baseUrl: 'http://localhost:8080/hrms/v1',
-    baseUrl8082: 'http://localhost:8082/hrms/v1',
-    baseUrl8083: 'http://localhost:8083/hrms/v1/',
-    baseUrl8080: 'http://deliziahruat.paysquare.com:8080/hrms/v1/',
-    apiBaseUrl: 'http://localhost:8085/hrms/v1/',
-    baseUrl8086: 'http://localhost:8086/hrms/v1/',
-    baseUrl8084: 'http://localhost:8084/hrms/v1/'
-};
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
-=======
     baseUrl8080: 'http://dev.deliziahr.com:8080/hrms/v1/',
     baseUrl8082: 'http://dev.deliziahr.com:8082/hrms/v1/',
     baseUrl8083: 'http://dev.deliziahr.com:8083/hrms/v1/',
@@ -33,4 +15,3 @@ exports.environment = {
     baseUrl8090: 'http://dev.deliziahr.com:8090/hrms/v1/',
     baseUrl8091: 'http://dev.deliziahr.com:8091/hrms/v1/'
 };
->>>>>>> Stashed changes
