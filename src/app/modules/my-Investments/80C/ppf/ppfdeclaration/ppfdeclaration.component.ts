03cc25d64320f94364f9b0dd88d0e3de05ac0bb8
AishwaryaMalviya <aishwarya.malviya@paysquare.com>
2021-03-31 12:34:43 +0530

    PPF Remaining Bug Solve

--- src/app/modules/my-Investments/80C/ppf/ppfdeclaration/ppfdeclaration.component.ts
+++ src/app/modules/my-Investments/80C/ppf/ppfdeclaration/ppfdeclaration.component.ts
@@ -918,6 +918,8 @@ export class PPFDeclarationComponent implements OnInit {
     this.alertService.sweetalertError(
       'Receipt Amount should be equal or greater than Actual Amount of Selected lines',
     );
+    this.receiptAmount = '0.00';
+      return false;
   } else if (receiptAmount_ > globalSelectedAmount_) {
     console.log(receiptAmount_);
     console.log(globalSelectedAmount_);
