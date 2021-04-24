fe60760f795be9338d1d73d53ce797e47946a10f
Rakesh Jain <ithelpdesk@paysquare.com>
2021-04-24 13:47:34 +0000

    environment.ts edited online with Bitbucket

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   baseUrl8080: 'https://test.deliziahr.com:8080/hrms/v1/',
+  baseUrl8081: 'https://test.deliziahr.com:8081/hrms/v1/',
   baseUrl8082: 'https://test.deliziahr.com:8082/hrms/v1/',
   baseUrl8083: 'https://test.deliziahr.com:8083/hrms/v1/',
   baseUrl8084: 'https://test.deliziahr.com:8084/hrms/v1/',
