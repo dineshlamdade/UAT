9ebeaa7d9c883bcaae5c4c7ba73b01cbc8315dcd
Shubham_Somawanshi <shubham.somwanshi@paysquare.com>
2021-08-31 14:21:57 +0530

    Excel js file code change

--- src/app/modules/companysetting/user-rolesand-permission/excel_service/excelservice.service.ts
+++ src/app/modules/companysetting/user-rolesand-permission/excel_service/excelservice.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
-import { Workbook } from 'exceljs'; 
+// import { Workbook } from 'exceljs'; 
 import * as fs from 'file-saver';
 
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
@@ -17,48 +17,48 @@ export class ExcelserviceService {
   public exportAsExcelFile(json: any[], excelFileName: string, sheetname: string,header): void {
 
     //Excel Title, Header, Data
-    const data = json;
-    //Create workbook and worksheet
-    let workbook = new Workbook();
-    let worksheet = workbook.addWorksheet(excelFileName);
-    //Add Header Row
-    let headerRow = worksheet.addRow(header);
-    // Cell Style : Fill and Border
-    headerRow.eachCell((cell, number) => {
-      cell.fill = {
-        type: 'pattern',
-        pattern: 'solid',
-        fgColor: { argb: 'FFFFFF00' },
-        bgColor: { argb: 'FF0000FF' }
-      }
-      cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
-    })
-    // Add Data and Conditional Formatting
-    data.forEach((element) => {
-      let eachRow = [];
-      header.forEach((headers) => {
-        eachRow.push(element[headers])
-      })
-      if (element.isDeleted === "Y") {
-        let deletedRow = worksheet.addRow(eachRow);
-        deletedRow.eachCell((cell, number) => {
-          cell.font = { name: 'Calibri', family: 4, size: 11, bold: false, strike: true };
-        })
-      } else {
-        worksheet.addRow(eachRow);
-      }
-    })
-    worksheet.getColumn(1).width = 20;
-    worksheet.getColumn(2).width = 20;
-    worksheet.getColumn(3).width = 30;
-    worksheet.getColumn(4).width = 30;
-    worksheet.getColumn(5).width = 20;
-    worksheet.addRow([]);
-    //Generate Excel File with given name
-    workbook.xlsx.writeBuffer().then((data) => {
-      let blob = new Blob([data], { type: EXCEL_TYPE });
-      fs.saveAs(blob, excelFileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
-    })
+    // const data = json;
+    // //Create workbook and worksheet
+    // let workbook = new Workbook();
+    // let worksheet = workbook.addWorksheet(excelFileName);
+    // //Add Header Row
+    // let headerRow = worksheet.addRow(header);
+    // // Cell Style : Fill and Border
+    // headerRow.eachCell((cell, number) => {
+    //   cell.fill = {
+    //     type: 'pattern',
+    //     pattern: 'solid',
+    //     fgColor: { argb: 'FFFFFF00' },
+    //     bgColor: { argb: 'FF0000FF' }
+    //   }
+    //   cell.border = { top: { style: 'thin' }, left: { style: 'thin' }, bottom: { style: 'thin' }, right: { style: 'thin' } }
+    // })
+    // // Add Data and Conditional Formatting
+    // data.forEach((element) => {
+    //   let eachRow = [];
+    //   header.forEach((headers) => {
+    //     eachRow.push(element[headers])
+    //   })
+    //   if (element.isDeleted === "Y") {
+    //     let deletedRow = worksheet.addRow(eachRow);
+    //     deletedRow.eachCell((cell, number) => {
+    //       cell.font = { name: 'Calibri', family: 4, size: 11, bold: false, strike: true };
+    //     })
+    //   } else {
+    //     worksheet.addRow(eachRow);
+    //   }
+    // })
+    // worksheet.getColumn(1).width = 20;
+    // worksheet.getColumn(2).width = 20;
+    // worksheet.getColumn(3).width = 30;
+    // worksheet.getColumn(4).width = 30;
+    // worksheet.getColumn(5).width = 20;
+    // worksheet.addRow([]);
+    // //Generate Excel File with given name
+    // workbook.xlsx.writeBuffer().then((data) => {
+    //   let blob = new Blob([data], { type: EXCEL_TYPE });
+    //   fs.saveAs(blob, excelFileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+    // })
 
   
     // const myworksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
@@ -78,6 +78,10 @@ export class ExcelserviceService {
     // }
     // myworksheet['A1'].s = { fill: { fgColor: { rgb: "#d6c227" } } };
 
+    const myworksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(json);
+    const myworkbook: XLSX.WorkBook = { Sheets: { 'data': myworksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = XLSX.write(myworkbook, { bookType: 'xlsx', type: 'array' });
+    this.saveAsExcelFile(excelBuffer, excelFileName);
   }
 
   private saveAsExcelFile(buffer: any, fileName: string): void {
