b6c0fc377a0ce54be669103f34355d4dc12570d2
dev76 <nilam.maner@paysquare.com>
2021-09-02 14:53:05 +0530

    environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,9 @@ export const environment = {
   baseUrl8080: 'https://dev.deliziahr.com:8080/hrms/v1/',
   baseUrl8082: 'https://dev.deliziahr.com:8082/hrms/v1/',
   baseUrl8083: 'https://dev.deliziahr.com:8083/hrms/v1/',
- baseUrl8084: 'https://dev.deliziahr.com:8084/hrms/v1/',
- //baseUrl8084: 'http://localhost:8084/hrms/v1/',
+  baseUrl8084: 'https://dev.deliziahr.com:8084/hrms/v1/',
   baseUrl8085: 'https://dev.deliziahr.com:8085/hrms/v1/',
- //baseUrl8086: 'http://localhost:8086/hrms/v1/',
-baseUrl8086: 'https://dev.deliziahr.com:8086/hrms/v1/',
+  baseUrl8086: 'https://dev.deliziahr.com:8086/hrms/v1/',
   baseUrl8087: 'https://dev.deliziahr.com:8087/hrms/v1/',
   baseUrl8088: 'https://dev.deliziahr.com:8088/hrms/v1/',
   baseUrl8089: 'https://dev.deliziahr.com:8089/hrms/v1/',
@@ -15,5 +13,3 @@ baseUrl8086: 'https://dev.deliziahr.com:8086/hrms/v1/',
   baseUrl8091: 'https://dev.deliziahr.com:8091/hrms/v1/',
   baseUrl8092: 'https://dev.deliziahr.com:8092/hrms/v1/',
 };
-
-//http://localhost:8084/hrms/v1/Areamaster/getAllAreaMasters
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,11 +7,8 @@ export const environment = {
 
   baseUrl8083: 'https://dev.deliziahr.com:8083/hrms/v1/',
   baseUrl8084: 'https://dev.deliziahr.com:8084/hrms/v1/',
- // baseUrl8084: 'http://localhost:8084/hrms/v1/',
   baseUrl8085: 'https://dev.deliziahr.com:8085/hrms/v1/',
- //baseUrl8086: 'http://localhost:8086/hrms/v1/',
-baseUrl8086: 'https://dev.deliziahr.com:8086/hrms/v1/',
- //baseUrl8086: 'http://dev.deliziahr.com:8086/hrms/v1/',
+  baseUrl8086: 'https://dev.deliziahr.com:8086/hrms/v1/',
   baseUrl8087: 'https://dev.deliziahr.com:8087/hrms/v1/',
   baseUrl8088: 'https://dev.deliziahr.com:8088/hrms/v1/',
   baseUrl8089: 'https://dev.deliziahr.com:8089/hrms/v1/',
