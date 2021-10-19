a0b5a8ac9caeb00b9e2e76e6efbc2b051587408a
RuchaB <rucha.bhide@paysquare.com>
2021-10-19 15:39:33 +0530

    environment file changes

---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  production: false,
+  baseUrl8080: 'https://test.deliziahr.com:8080/hrms/v1/',
+  baseUrl8081: 'https://test.deliziahr.com:8081/hrms/v1/',
+
+  baseUrl8082: 'https://test.deliziahr.com:8082/hrms/v1/',
+
+  baseUrl8083: 'https://test.deliziahr.com:8083/hrms/v1/',
+  baseUrl8084: 'https://test.deliziahr.com:8084/hrms/v1/',
+  baseUrl8085: 'https://test.deliziahr.com:8085/hrms/v1/',
+  baseUrl8086: 'https://test.deliziahr.com:8086/hrms/v1/',
+  baseUrl8087: 'https://test.deliziahr.com:8087/hrms/v1/',
+  baseUrl8088: 'https://test.deliziahr.com:8088/hrms/v1/',
+  baseUrl8089: 'https://test.deliziahr.com:8089/hrms/v1/',
+  baseUrl8090: 'https://test.deliziahr.com:8090/hrms/v1/',
+  baseUrl8091: 'https://test.deliziahr.com:8091/hrms/v1/',
+  baseUrl8092: 'https://test.deliziahr.com:8092/hrms/v1/',
+
+  
+
+};
